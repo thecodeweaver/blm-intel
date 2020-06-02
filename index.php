90e5b09966303697b4ee5ba98771e9ff132ab8fc
TheCodeWeaver <thecodeweaver@protonmail.com>
2020-06-02 01:15:12 -0700

    Base HTML Code

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Tweet Intel</title>
+        <meta name=”viewport” content=”width=device-width, initial-scale=1″>
+    </head>
+
+    <body style="background-color:DimGray">
+        <h1>The Latest Tweets For the Following Hashtags:</h1>
+        <?php
+        # TODO: Hashtags from web app
+        # TODO: The actual tweets themselves (Use text alignment/CSS layout stuff to setup)
+        ?>
+
+        <footer style="font-size:200%">
+        Made with 💙 by TheCodeWeaver.<a href="https://github.com/thecodeweaver/tweet-intel/"> Source code</a>
+        </footer>
+    </body>
+</html>
