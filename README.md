518deac35a53a409cbdb5f19cccbacc3261339e3
TheCodeWeaver <thecodeweaver@protonmail.com>
2020-06-03 00:30:24 -0700

    Initial PHP Twitter API Code

--- README.md
+++ README.md
@@ -5,6 +5,13 @@ A web application that displays real time information by scraping tweets. Made w
 1. Follow instructions [here](https://www.linode.com/docs/applications/containers/how-to-install-docker-and-deploy-a-lamp-stack/) to install a Docker LAMP stack container.
 2. Run the following command to start the container and attach it to the project directory:
 `sudo docker run --name=php -v [DIRECTORY WHERE THIS PROJECT IS LOCATED]:/var/www/example.com/public_html/ -p 80:80 -t -i linode/lamp /bin/bash`
+3. Setup your environment variables according to the code
+`
+ACCESS_TOKEN=""
+ACCESS_TOKEN_SECRET=""
+CONSUMER_KEY=""
+CONSUMER_SECRET=""
+`
 3. Once the container is launched, run `service apache2 start` to start the Apache Web Server.
 4. Run `ifconfig` to determine the IP address of your container (so you can access the web server)
 5. Visit the IP address from step 4 to see the Tweet Intel page.
--- index.php
+++ index.php
@@ -2,18 +2,39 @@
 <html>
     <head>
         <title>Tweet Intel</title>
-        <meta name=”viewport” content=”width=device-width, initial-scale=1″>
+        <meta name=”viewport” content=”width=device-width, initial-scale=1″>$_ENV['YOUR_CONSUMER_KEY'],
     </head>
 
     <body style="background-color:DimGray">
         <h1>The Latest Tweets For the Following Hashtags:</h1>
         <?php
-        # TODO: Hashtags from web app
-        # TODO: The actual tweets themselves (Use text alignment/CSS layout stuff to setup)
-        echo "Hello World!";
+        require_once("TwitterAPIExchange.php");
+
+        $twitter_keys = array(
+            "oauth_access_token" => $_ENV["ACCESS_TOKEN"],
+            "oauth_access_token_secret" => $_ENV["ACCESS_TOKEN_SECRET"],
+            "consumer_key" => $_ENV["CONSUMER_KEY"],
+            "consumer_secret" => $_ENV["CONSUMER_SECRET"]
+        );
+
+        $search = $_ENV["SEARCH_TERM"]?:"twitter";
+        $tweet_count = $_ENV["TWEET_COUNT"]?:5; 
+
+        $twitter = new TwitterAPIExchange($twitter_keys);
+
+        echo $twitter->setGetfield(urlencode("q=" . $search))
+                    ->setGetfield(urlencode("result_type=recent"))
+                    ->setGetField(urlencode("count=" . $tweet_count))
+                    ->buildOath("https://api.twitter.com/1.1/search/tweets.json", "GET")
+                    ->performRequest();
+
+        # Grab 5 tweets from the search term
+        #for ($i = 0; $i < $tweet_count; $i++) {
+        #
+        #}
         ?>
 
-        <footer style="font-size:200%">
+        <footer style="font-size:150%">
         Made with 💙 by TheCodeWeaver.<a href="https://github.com/thecodeweaver/tweet-intel/"> Source code</a>
         </footer>
     </body>
