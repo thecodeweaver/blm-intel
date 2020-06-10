bdadc788a5dbea58c5a0eafe6c8b78c14e153857
TheCodeWeaver <thecodeweaver@protonmail.com>
2020-06-10 10:36:38 -0700

    Removed development keys

--- twitter.php
+++ twitter.php
@@ -4,21 +4,12 @@
 require_once("TwitterAPIExchange.php");
 
 function twitter_init() {
-    /*
+
     $twitter_keys = array(
     "oauth_access_token" => $_ENV["ACCESS_TOKEN"],
     "oauth_access_token_secret" => $_ENV["ACCESS_TOKEN_SECRET"],
     "consumer_key" => $_ENV["CONSUMER_KEY"],
     "consumer_secret" => $_ENV["CONSUMER_SECRET"]
-    );
-    */
-
-    # Harcoded values for development
-    $twitter_keys = array(
-    "oauth_access_token" => "1212840451243331584-8ho4q0YA6EFg8eaxx2zaWr0qQAFVrh",
-    "oauth_access_token_secret" => "c5YGftZUPE0cNtkWftOFaxEOmVtXU0HUBxwkAnx6Asr1T",
-    "consumer_key" => "Ba5vOdt1Qty2jngah42aKIq1f",
-    "consumer_secret" => "hslcAqjWosWd7YMkUBsa8P453K34YiCwhA3zUYdjbJ8CEf3PJB"
     );
 
     return new TwitterAPIExchange($twitter_keys);
