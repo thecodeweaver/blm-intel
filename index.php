ec32a423039ca178ca19781c0bce53d48a164a4a
TheCodeWeaver <thecodeweaver@protonmail.com>
2020-06-09 21:34:19 -0700

    CSS'd it up!

--- index.php
+++ index.php
@@ -4,24 +4,28 @@
         <title>BLM Alert</title>
         <meta name=”viewport” content=”width=device-width, initial-scale=1″>
         <meta charset="UTF-8">
+        <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+        <?php
+        # Init libraries, Twitter API
+        ?>
     </head>
 
-    <header>
-        <h1>BLM Alerts: Real time protest information and resources.</h1>
-        <a href="/index.php">Home</a>
-        <a href="/resources.html">Resources</a>
-    </header>
+    <body class="w3-container w3-pale-yellow w3-mobile">
 
-    <hr>
-    <body style="background-color:DimGray">
+        <header class="w3-blue-grey">
+            <h1>BLM Alerts: Real time protest information and resources.</h1>
+            <a href="/index.php">Home</a>
+            <a href="/resources.html">Resources</a>
+        </header>
+        <hr>
+
         <h3>Note: this website is far from finished.</h2>
-        <p style="font-size:100%">
+        <p>
         The real-time information part of the site is a bit inconsistent, so check out the resources page below if you want to help out in other ways.
         Also, I apologize for the poor formatting, designing web pages is not my strength. I'll probably fix it over the next few days.
         Finally, this is not so mobile friendly yet, so I apologize to people on their phones. This is also something I'll hopefully fix soon.
         </p>
 
-        <!--TODO: Format this nicely-->
         <h4>Locations</h4>
         <a href="#LosAngeles">Los Angeles</a>
         <a href="#ElSegundo">El Segundo</a>
@@ -30,25 +34,28 @@
         <section id="LosAngeles">
         <?php
         # Code for Los Angeles location
+        # Put in a list (https://www.w3schools.com/w3css/w3css_lists.asp)
         ?>
         </section>
 
         <section id="ElSegundo">
         <?php
         # Code for El Segundo location
+        # Put in a list (https://www.w3schools.com/w3css/w3css_lists.asp)
         ?>
         </section>
 
         <section id="Hawthorne">
         <?php
         # Code for Hawthorne location
+        # Put in a list (https://www.w3schools.com/w3css/w3css_lists.asp)
         ?>
         </section>
 
-    </body>
+        <hr>
+        <footer class="w3-blue-grey">
+        Made with 💙 by <a href="https://github.com/thecodeweaver/">TheCodeWeaver</a>
+        </footer>
 
-    <hr>
-    <footer style="font-size:120%">
-        Made with 💙 by TheCodeWeaver. <a href="https://github.com/thecodeweaver/tweet-intel/">Source code</a>
-    </footer>
+    </body>
 </html>
